a259385859952b0d57fc45b05385e5e86c016c1b
ludabi04 <ludabi@live.com>
2023-01-09 10:09:39 -0300

    modificando alert

--- js/calcularArancel.js
+++ js/calcularArancel.js
@@ -1,7 +1,7 @@
 let consulta =Number(prompt("Cuantos días queres venir por semana?"));
 
 function cantidadDias () {
-    let resultado = alert("el arancel mensual seria de " + (consulta*4*210))
+    let resultado = alert("el arancel mensual seria de " + "$" + (consulta*4*210))
 }
 cantidadDias();
 
