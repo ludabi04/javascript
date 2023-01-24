9567803a5dff010813144228c2bef9b1df13d131
ludabi04 <ludabi@live.com>
2023-01-24 12:11:03 -0300

    corrigiendo codigo de else en consultaDias.js

--- js/consultaDias.js
+++ js/consultaDias.js
@@ -1,36 +1,36 @@
  let nombre = prompt("Indicanos tu nombre?")
- let diasActividades = prompt("Que día queres venir al gimnasio?")
+ let diasActividades = prompt("Que día queres venir al gimnasio?").toUpperCase()
  
-    if (diasActividades == "lunes" || diasActividades == "Lunes" || diasActividades == "LUNES"){
+    if (diasActividades == "LUNES"){
     
         document.write("BIENVENIDO " + nombre + " te pasamos los horarios de actividades de los dias: " + diasActividades + "</br> </br> -08:00 - Musculacion<br> -09:00 - Zumba </br> -11:00 - Yoga<br> -12:00 - Stretching </br> -14:00 - Aerolocal<br> -16:00 - Aerobox </br> -18:00 - Yoga<br> -20:00 - Stretching ")
         }else if 
-            (diasActividades == "martes" || diasActividades == "Martes" || diasActividades == "MARTES"){
-    
-        document.write("BIENVENIDO " + nombre + " te pasamos los horarios de actividades de los dias: " + diasActividades + "</br> </br> -08:00 - Musculacion<br> -09:00 - Zumba </br> -11:00 - Yoga<br> -12:00 - Stretching </br> -14:00 - Aerolocal<br> -16:00 - Aerobox </br> -18:00 - Yoga<br> -20:00 - Stretching ")
-
-        }else if 
-            (diasActividades == "miercoles" || diasActividades == "Miercoles" || diasActividades == "MIERCOLES" || diasActividades == "miércoles" || diasActividades == "Miércoles" || diasActividades == "MIÉRCOLES"){
+            (diasActividades == "MARTES"){
     
         document.write("BIENVENIDO " + nombre + " te pasamos los horarios de actividades de los dias: " + diasActividades + "</br> </br> -08:00 - Musculacion<br> -09:00 - Zumba </br> -11:00 - Yoga<br> -12:00 - Stretching </br> -14:00 - Aerolocal<br> -16:00 - Aerobox </br> -18:00 - Yoga<br> -20:00 - Stretching ")
 
         }else if 
-            (diasActividades == "jueves" || diasActividades == "Jueves" || diasActividades == "JUEVES"){
+            (diasActividades == "MIERCOLES" || diasActividades == "MIÉRCOLES"){
     
         document.write("BIENVENIDO " + nombre + " te pasamos los horarios de actividades de los dias: " + diasActividades + "</br> </br> -08:00 - Musculacion<br> -09:00 - Zumba </br> -11:00 - Yoga<br> -12:00 - Stretching </br> -14:00 - Aerolocal<br> -16:00 - Aerobox </br> -18:00 - Yoga<br> -20:00 - Stretching ")
 
         }else if 
-            (diasActividades == "viernes" || diasActividades == "Viernes" || diasActividades == "VIERNES"){
+            (diasActividades == "JUEVES"){
+    
+        document.write("BIENVENIDO " + nombre + " te pasamos los horarios de actividades de los dias: " + diasActividades + "</br> </br> -08:00 - Musculacion<br> -09:00 - Zumba </br> -11:00 - Yoga<br> -12:00 - Stretching </br> -14:00 - Aerolocal<br> -16:00 - Aerobox </br> -18:00 - Yoga<br> -20:00 - Stretching ")
+
+        }else if 
+            (diasActividades == "VIERNES"){
     
         document.write("BIENVENIDO " + nombre + " te pasamos los horarios de actividades de los dias: " + diasActividades + "</br> </br> -08:00 - Musculacion<br> -09:00 - Zumba </br> -11:00 - Yoga</br> -12:00 - Stretching </br> -14:00 - Aerolocal</br> -16:00 - Aerobox </br> -18:00 - Yoga<br> -20:00 - Stretching ")
 
         }else if 
-            (diasActividades == "sabado" || diasActividades == "Sabado" || diasActividades == "SABADO" || diasActividades == "sábado" || diasActividades == "Sábado" || diasActividades == "SÁBADO"){
+            (diasActividades == "SABADO" || diasActividades == "SÁBADO"){
     
         alert("BIENVENIDO " + nombre + " te comentamos que los dias: " + diasActividades + " el gimnasio permanence cerrado")
 
         }else if 
-            (diasActividades == "domingo" || diasActividades == "Domingo" || diasActividades == "DOMINGO"){
+            (diasActividades == "DOMINGO"){
     
         alert("BIENVENIDO " + nombre + " te comentamos que los dias: " + diasActividades + " el gimnasio permanence cerrado")
 
